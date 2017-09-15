372140857851024792b1d719e1f66f691d818534
Mallikarjuna Kalkere <mkalkere@gmail.com>
2017-09-15 15:14:04 -0400

    INotifyPropertyChanged Style2

---
+++ INotifyPropertyChanged-Style2/CustomCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace INotifyPropertyChanged_Style2
+{
+    public class CustomCommand : ICommand
+    {
+
+        Action<object> execute;
+        Predicate<object> canExecute;
+
+        public CustomCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null ? true : canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
---
+++ INotifyPropertyChanged-Style2/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace INotifyPropertyChanged_Style2
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        #region Constructor
+        public MainWindowViewModel()
+        {
+            this.Name = "Initial Name";
+            InitializeCommands();
+        }
+
+        
+        #endregion
+
+        #region Properties
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                RaisePropertyChanged(nameof(Name));
+            }
+        }
+
+        #endregion
+
+        #region ICommand
+        public ICommand Click { get; set; }
+        private void InitializeCommands()
+        {
+            Click = new CustomCommand(OnClick, CanClick);
+        }
+
+        private bool CanClick(object obj)
+        {
+            return true;
+        }
+
+        private void OnClick(object obj)
+        {
+            Name = "New Name " + new Random().Next(1, 100);
+        }
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+    }
+}
